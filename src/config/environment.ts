cb0bf333558a65e264aa237333774b3c88afcc2f
Jessa <jessamarie@users.noreply.github.com>
2020-09-07 19:52:34 -0400

    Utilize overnightJS to create server and setup routes; create models for schema; create services for business logic; convert environments; type-safe seeding

---
+++ src/config/environment.ts
@@ -0,0 +1,17 @@
+import production from './env/production';
+import development from './env/development';
+import test from './env/test';
+import { Logger } from '@overnightjs/logger';
+
+const env = process.env.NODE_ENV;
+
+Logger.Imp(`Running in ${process.env.NODE_DEV}`);
+
+const configs = [
+    development,
+    test,
+    production
+]
+const config = configs.find(config => config.env === env) || development;
+
+export default config;
