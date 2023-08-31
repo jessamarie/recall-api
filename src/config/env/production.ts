6017a421e7cc6fc76d7a6b6b7673145783c7ed9a
Jessamarie <jessamarie@users.noreply.github.com>
2023-08-31 12:45:13 -0400

    Add dotenv import to read env variables

--- src/config/env/production.ts
+++ src/config/env/production.ts
@@ -1,13 +1,14 @@
 import { EnvironmentType, IConfigProperties } from './config.model';
+import 'dotenv/config';
 
 const production: IConfigProperties = {
-    env: EnvironmentType.PROD,
-    app: {
-        port: parseInt(process.env.PORT as string)
-    },
-    mongoDB: {
-        url: process.env.MONGO_URL as string
-    }
+  env: EnvironmentType.PROD,
+  app: {
+    port: parseInt(process.env.PORT as string)
+  },
+  mongoDB: {
+    url: process.env.MONGO_URL as string
+  }
 };
 
 export default production;
--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,6 +2,7 @@ import production from './env/production';
 import development from './env/development';
 import test from './env/test';
 import Logger from 'jet-logger';
+import 'dotenv/config';
 
 const env = process.env.NODE_ENV;
 
